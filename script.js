5a479e34bd350092196c6e1e45e66a6de6a22890
Noah Calmette <shaikeerr@gmail.com>
2023-10-24 09:26:57 +0200

    Parse des données dans la page

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
 
-
+let tab = []
 
 function annee(x) {
-fetch('data.json').then(function(response) {
-    response.json().then(function(data) {              //Récupère les données du JSON et les stocke dans une variable data
-        console.log(data);
-        const element = data[1]   //Execute la fonction analogies avec toutes les valeurs de data
-        reponse = document.querySelector(".page")
-        reponse.innerHTML += element
-    })       
-})
+
+//Permet de récupérer toutes les données d'un fichier JSON et de les stocker dans une variable data
+d3.json("data.json" ).then(function(data) {
+
+    //Permet de récupérer l'objet de données correspondant à l'année inscrite dans la fonction et de le stocker dans une variable element 
+    let current_year = data["classement"][x]
+        d3.select("body")
+        .selectAll(".barre")
+        .data(current_year)
+        .join("div")
+        .attr("class", "barre")
+        .style("background-color", d => d.couleur )
+        .html((d,i) => `Position : ${d.position} <br> Jeu : ${d.jeu} <br> Source: ${d.lien} <br> Argent généré : ${d.argent_generé} <br> Nombre de joueurs : ${d.nombre_joueurs} <br> Nombre de Tournois : ${d.nombre_tournois} <br> Couleur : ${d.couleur} <br><br>` );
+
+ });
 
 }
+
+annee(2023)
